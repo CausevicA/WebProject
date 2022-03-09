e3d699404c49add00a855d447ee7c5f750b52b4b
CausevicA <amila.causevic@stu.ibu.edu.ba>
2022-03-09 12:06:33 +0100

    Hi

---
+++ .gitignore/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '55f9387503d5cf9d465a6d066767048f6803c630',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '55f9387503d5cf9d465a6d066767048f6803c630',
+            'dev_requirement' => false,
+        ),
+    ),
+);
